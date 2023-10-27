c4993b0ddc392efe8de27fe6bb7815611a7ac3c5
minhnv60 <minhnv60@fpt.com.vn>
2023-10-27 11:28:15 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  setTimeout(() => {
+    window.location.reload()
+  }, 800000)
+})
+</script>
+
+<template>
+  <iframe
+    src="https://nvmportfolio.netlify.app/blogs"
+    frameborder="0"
+    style="width: 100vw; height: 100vh; max-width: 100%; margin: auto"
+  ></iframe>
+</template>
+
+<style scoped></style>
